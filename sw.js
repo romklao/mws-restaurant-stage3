53aafb57fbac17a06183d10f424b1846cc89ddd5
Romklao <rk120879@gmail.com>
2018-05-09 14:42:12 -0700

    feat: Add an event 'activate' to remove the old cache

--- sw.js
+++ sw.js
@@ -39,6 +39,22 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+                 cacheName != staticCacheName;
+        })
+        .map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
