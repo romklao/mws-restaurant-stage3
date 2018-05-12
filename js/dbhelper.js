8c14354b99a1e031bfa53747ab0e0f8865da221b
Romklao <rk120879@gmail.com>
2018-05-12 00:29:49 -0700

    style: Add comment for ServiceWorker function

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -170,6 +170,9 @@ class DBHelper {
   }
 }
 
+/* Register ServiceWorker to cache data for the site
+ * to allow any page that has been visited is accessible offline
+ */
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
