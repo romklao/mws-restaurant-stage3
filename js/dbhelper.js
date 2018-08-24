1bccbae91c25321ad684e7bb647081bc42ff2a44
Romklao <rk120879@gmail.com>
2018-08-23 22:03:58 -0700

    feat: Change the server url fetch data from the server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -12,8 +12,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 1337;// Change this to your server port
-    return `http://localhost:${port}`;
+    //const port = 1337;// Change this to your server port
+    //return `https://restaurant-reviews-api.herokuapp.com/:${port}`;
+    return 'https://restaurant-reviews-api.herokuapp.com';
   }
 
   /**
